c2a250101827ee2a1465a9e0aeba2e030b4accef
sa <spiritbro59@yandex.com>
2018-11-03 08:20:22 +0000

    bismillah

--- panda.js
+++ panda.js
@@ -16,37 +16,65 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.get('/', function(req, res){
-    if(!req.session.username){
- res.redirect(`/login`);
-    }else{
+req.session.username=""
 res.render('index',{"username":req.session.username})
 //   res.sendFile(__dirname + '/index.html');
-    }
+
 
 });
-app.get('/login', function(req, res){
-if(req.session.username){
- res.redirect(`/`);
-    }else{
+// app.get('/login', function(req, res){
+// if(req.session.username){
+//  res.redirect(`/`);
+//     }else{
 
-  res.sendFile(__dirname + '/login.html');
-    }
 //   res.sendFile(__dirname + '/login.html');
-});
+//     }
+// //   res.sendFile(__dirname + '/login.html');
+// });
 app.post('/logins', function(req, res){
     req.session.username=req.body.username
+    res.send("sukses")
 // res.json(req.body.hoho)
-res.redirect(`/`);
+// res.redirect(`/`);
 //   res.sendFile(__dirname + '/login.html');
 });
-app.get('/logout',(req,res)=>{
-    req.session.destroy();
-})
+// app.get('/logout',(req,res)=>{
+//     req.session.destroy();
+// })
+var jumlah=[];
+
 io.on('connection', function(socket){
+
+    socket.on("add user",(msg)=>{
+
+        var data=typeof msg=="string"?JSON.parse(msg):msg
+
+jumlah.push({socket:socket,username:data.username})
+        io.emit('add user', {jumlah:jumlah.length,username:data.username});
+    })
+     socket.on('disconnect', function() {
+      console.log('Got disconnect!');
+// console.log
+
+index = jumlah.findIndex(x => x.socket==socket);
+
+if(index>-1){
+io.emit("remove user",{username:jumlah[index].username,jumlah:jumlah.length-1})
+jumlah.splice(index, 1);
+
+}
+
+
+   });
    socket.on('chat message', function(msg){
     // console.log(msg);
 
      io.emit('chat message', typeof msg=="string"?JSON.parse(msg):msg);
+  });
+  socket.on('keypress', function(msg){
+    // console.log(msg);
+
+     io.emit('keypress', typeof msg=="string"?JSON.parse(msg):msg);
   });
 });
 
